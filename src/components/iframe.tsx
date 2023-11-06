aee74ae4127d9a0c99ace95843890883b4b2ffc6
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-06 11:15:51 -0600

    6Nov2023

--- src/components/iframe.tsx
+++ src/components/iframe.tsx
@@ -1,7 +1,7 @@
 export default function MyIframe() {
   return (
     <iframe
-      src="https://parent-rev-proxy.vercel.app/iframe/root"
+      src="https://parent-rev-proxy.vercel.app/proxy"
       className="h-80 w-full"
     ></iframe>
   );
